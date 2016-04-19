18edb84bf83e6b9f06e8997cc31ee8c1b85b37c2
Emmanuel Maldonado <emaldonado@innomius.com>
2016-04-19 11:47:48 -0500

    finish configuration test

--- tests.webpack.js
+++ tests.webpack.js
@@ -6,8 +6,6 @@ require('core-js/es5');
 var context = require.context('./app', true, /-test\.js$/);
 context.keys().forEach(context);
 
-
-/*
 // Create a Webpack require context so we can dynamically require our
 // project's modules. Exclude test files in this context.
 var projectContext = require.context('./app/', true, /^((?!__tests__).)*.js?$/);
@@ -19,4 +17,3 @@ beforeEach(() => {
   // Remove our modules from the require cache before each test case.
   projectModuleIds.forEach(id => delete require.cache[id]);
 });
-*/
