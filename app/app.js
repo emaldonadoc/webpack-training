56c47c58524e69dba07bf01f21a6333606616b7f
Emmanuel Maldonado <emaldonado@innomius.com>
2017-03-28 23:07:22 -0600

    update and upgrade react react-dom react-datagrid version

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Table from "./components/tableDatagrid/tableDatagrid";
-import ReactDom from "react-dom";
+import React from "react"
+import ReactDom from "react-dom"
+import Table from "./components/tableDataGrid/table-datagrid-component"
 
 let data = [
   { id: 0, index: 1, firstName: 'John', city: 'London', email: 'jon@gmail.com'},
   { id: 1, index: 1, firstName: 'John', city: 'London', email: 'jon@gmail.com'},
-  { id: 2, index: 1, firstName: 'John', city: 'London', email: 'jon@gmail.com'}  
+  { id: 2, index: 1, firstName: 'John', city: 'London', email: 'jon@gmail.com'}
 ];
 
 ReactDom.render(<Table renderData={data}/>, document.getElementById('app') );
