ec6889780c3332c15a83eb8eb89148f16218eeb8
Emmanuel Maldonado <emaldonado@innomius.com>
2017-03-30 17:11:54 -0600

    Add superagent and prepare component to read from state

--- app/components/main/main-component.js
+++ app/components/main/main-component.js
@@ -3,14 +3,24 @@ import React,{Component} from 'react'
 export default class Main extends Component {
   constructor(props){
     super(props)
+
+    this.state = {
+      people: []
+    }
   }
 
   render(){
+    let list = []
+    this.state.people.forEach( (person)=> {
+      list.push( <li>{person.firstName}, {person.lastName}</li>)
+    });
     return (
       <div>
-        <p> Main component</p>
+        <ul>
+          {list}
+        </ul>
       </div>
-    )    
+    )
   }
 
 }
