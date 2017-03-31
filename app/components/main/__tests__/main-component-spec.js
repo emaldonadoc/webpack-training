5c344d9b3ffa961579d834a891d5152539e4d201
Emmanuel Maldonado <emaldonado@innomius.com>
2017-03-31 15:44:38 -0600

    Main component test

---
+++ app/components/main/__tests__/main-component-spec.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import TestUtils from 'react-dom/lib/ReactTestUtils';
+import Main from '../main-component'
+
+describe( 'Main Spec', () => {
+
+  it('Main content with default state "people:[]"', () =>{
+    let main = TestUtils.renderIntoDocument(<Main />);
+    let people = TestUtils.scryRenderedDOMComponentsWithClass(main, 'person');
+    expect(main).toBeTruthy();
+    expect(people.length).toEqual(0);
+  })
+
+  it('Main content update state ', () =>{
+    let main = TestUtils.renderIntoDocument(<Main />);
+    let peopleData = {people:[{firstName:"death", lastName:"maldonado"}]};
+
+    main.setState(peopleData);
+
+    let people = TestUtils.scryRenderedDOMComponentsWithClass(main, 'person');
+    expect(people.length).toEqual(1);
+  });
+
+})
