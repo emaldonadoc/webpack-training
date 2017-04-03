b98d6492bb7ca9d677d35bc148804e621170395a
Emmanuel Maldonado <emaldonado@innomius.com>
2017-04-02 22:14:40 -0500

    Init sinon implementation

--- app/components/main/__tests__/main-component-spec.js
+++ app/components/main/__tests__/main-component-spec.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react'
-import TestUtils from 'react-dom/lib/ReactTestUtils';
+import TestUtils from 'react-dom/lib/ReactTestUtils'
 import Main from '../main-component'
+import sinon from 'sinon'
 
 describe( 'Main Spec', () => {
+
+  var server;
+
+  beforeEach(function() {
+    server = sinon.fakeServer.create();
+    server.respondWith("GET", "http://localhost:9999/people",
+                       [200, { "Content-Type": "application/json;charset=UTF-8" },
+                       '{"people":[{"firstName":"death", "lastName":"maldonado"},{"firstName":"home", "lastName":"home"}]']);
+  });
+
+  afterEach(function () {
+    server.restore();
+  });
 
   it('Main content with default state "people:[]"', () =>{
     let main = TestUtils.renderIntoDocument(<Main />);
@@ -13,10 +27,6 @@ describe( 'Main Spec', () => {
 
   it('Main content update state ', () =>{
     let main = TestUtils.renderIntoDocument(<Main />);
-    let peopleData = {people:[{firstName:"death", lastName:"maldonado"},{firstName:"home", lastName:"home"}]};
-
-    main.setState(peopleData);
-
     let people = TestUtils.scryRenderedDOMComponentsWithClass(main, 'person');
     expect(people.length).toEqual(2);
   });
